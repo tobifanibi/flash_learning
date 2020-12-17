99393b66c1eff26905f378b1517d41297613ed3a
Asa Holland <hollandasa@gmail.com>
2020-12-17 04:43:46 -0800

    Introduce templates
    Added a templates folder, index.html template file and added template support onto the main app.py file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 
-# Import the Flask class
-from flask import Flask
+# Import the Flask class (for the main app) and render_templates to run routes based on templates
+from flask import Flask, render_template
 
 # Starts a new Flask app using this app file
 app = Flask(__name__)
@@ -9,7 +9,9 @@ app = Flask(__name__)
 # Start a new route for the index page
 @app.route('/')
 def index():
-	return "Hello team penguin"
+
+	# User render templates to use the index.html file as a template for the index page
+	return render_template('index.html')
 
 # When app is run, start the Flask app on the localhost server (port 5000)
 if __name__ == "__main__":
