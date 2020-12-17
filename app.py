25dcbab5b1235a8f186cf096fcf2cd96d3ca64d3
Asa Holland <hollandasa@gmail.com>
2020-12-17 05:42:30 -0800

    Update app.py
    Added SQLAlchemy and config files to allow creation of models based on SQLAlchemy database entries.
    
    Added a model for a Flashcard, although this will likely need to be moved to a separate models folder.

--- app.py
+++ app.py
@@ -5,8 +5,45 @@
 # Import url_for in order to link stylesheets in the base jinja2 template
 from flask import Flask, render_template, url_for
 
+# Import SQLAlchemy to use backend database
+from flask_sqlalchemy import SQLAlchemy
+
+# imported to track date of creation of flashcards
+from datetime import datetime
+
+
 # Starts a new Flask app using this app file
 app = Flask(__name__)
+
+# Add config file specific for the database location (test db)
+# This uses SQL Lite which is a very simple database with no frills
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+# Add additional config file to stop tracking modifications to SQL Alchemy, this isn't necessary, but removes a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the database with the settings used in our app
+db = SQLAlchemy(app)
+
+
+# Create a model for a Flashcard
+class Flashcard(db.Model):
+
+	# Add a primary key with a id field
+	id = db.Column(db.Integer, primary_key=True)
+
+	# Add a column to the database with the string of the front side of our card
+	content_front_side = db.Column(db.String(200), nullable=False)
+
+	# Add a column to the database with the string of the back side of our card
+	content_back_side = db.Column(db.String(200), nullable=False)
+
+	# Add date of creation for bookeeping, with a default creation time of whenever it was created
+	date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+	# Add a repprint function just to make it easier to reference this flashcard
+	def __repr__(self):
+		return '<Flashcard %r>' % self.id
 
 # Start a new route for the index page
 @app.route('/')
