a99d3349a1798823643dffc653ca21990985a2c4
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2020-12-18 15:50:21 -0500

    Fix Internal Server Error bug when trying to login/register.

--- flash_learning/main/routes.py
+++ flash_learning/main/routes.py
@@ -1,63 +1,120 @@
 import os
-from flask import Blueprint, render_template
-from flask_login import login_user,current_user
-from flash_learning.models.users import User
+from flask import Blueprint, flash, redirect, render_template, url_for
+from flask_login import login_user, current_user, logout_user
+from werkzeug.urls import url_parse
+
+from flash_learning.models.user import User
 from flash_learning.models.forms import LoginForm,SignupForm
 from flash_learning import db
 from flask import request
-main = Blueprint("main", __name__)
 
+main = Blueprint("main", __name__)
 
 
 @main.route('/', methods=["GET", "POST"])
 def index():
+    """The app's landing page."""
+
     return render_template("index.html")
 
 
-@main.route('/login', methods=['GET', 'POST'])
+@main.route('/login', methods=["GET", "POST"])
 def login():
-	if current_user.is_authenticated==False:
-		form = LoginForm()
-		return render_template('login.html', form=form)
-		if form.validate_on_submit():
-			user = User(form.username.data)
-			user.alternative_id=os.urandom(16)
-			user.authenticated = True
-			login_user(user, remember=True)
-			return render_template("index.html")
-	else:
-		return render_template("index.html")
+    """"Student login page."""
+
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
+    form = LoginForm()
+
+    # Redirect the student to their home page if username and password are correct, otherwise stay at the login page.
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("main.login"))
+        login_user(user)
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for("main.index")
+        return redirect(next_page)
+
+    return render_template("login.html", title="Sign In", form=form)
+
+    # Something here is causing an internal server error when trying to load the login page.
+    """
+    if current_user.is_authenticated==False:
+        form = LoginForm()
+        return render_template('login.html', form=form)
+        if form.validate_on_submit():
+            user = User(form.username.data)
+            user.alternative_id=os.urandom(16)
+            user.authenticated = True
+            login_user(user, remember=True)
+            return render_template("index.html")
+    else:
+        return render_template("index.html")
+    """
+
+
+@main.route("/logout")
+def logout():
+    """Logout the current user."""
+
+    logout_user()
+
+    return redirect(url_for("main.index"))
+
 
 @main.route('/signup',methods=['POST','GET'])
 def signup():
-	form = SignupForm()
-	if request.form.get('email')==None and request.form.get('username')==None and request.form.get('password')==None and request.form.get('grade')==None and request.form.get('school')==None:
-		print("Console User Entering Signup Page")
-		return render_template("signup.html", form=form)
+    if current_user.is_authenticated:
+        return redirect(url_for("main.index"))
 
+    form = SignupForm()
 
-	if form.validate_on_submit()==False:
-		print("Console User Sign Up Invalid")
-		form.msg="Please Enter ALl Required Info"
-		return render_template("signup.html", form=form)
-	else:
-		print("Checking if user Exist")
-		email = request.form.get('email')
-		username = request.form.get('username')
-		password = request.form.get('password')
-		grade = request.form.get('grade')
-		school = request.form.get('school')
-		user = User.query.filter_by(email=email).first()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,
+                    email=form.email.data,
+                    school=form.school.data,
+                    grade=form.grade.data)
 
-	if user:  # if a user is found, we want to redirect back to signup page so user can try again
-		print("user exist")
-		form = SignupForm()
-		form.msg="A User With This Password or Username Exist"
-		return render_template("signup.html", form=form)
-	else:
-		print("Adding User to Database")
-		#password should be hashed
-		new_user = User(email=email, username=username, password=password,grade=grade,school=school)
-		db.session.add(new_user)
-		db.session.commit()
-		return index()
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Welcome to Flash Learning!")
+        return redirect(url_for("main.login"))
+
+    return render_template("signup.html", title="Sign Up", form=form)
+
+    """
+    if request.form.get('email')==None and request.form.get('username')==None and request.form.get('password')==None and request.form.get('grade')==None and request.form.get('school')==None:
+        print("Console User Entering Signup Page")
+        return render_template("signup.html", form=form)
+
+    if form.validate_on_submit()==False:
+        print("Console User Sign Up Invalid")
+        form.msg="Please Enter ALl Required Info"
+        return render_template("signup.html", form=form)
+    else:
+        print("Checking if user Exist")
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        grade = request.form.get('grade')
+        school = request.form.get('school')
+        user = User.query.filter_by(email=email).first()
+
+    if user:  # if a user is found, we want to redirect back to signup page so user can try again
+        print("user exist")
+        form = SignupForm()
+        form.msg="A User With This Password or Username Exist"
+        return render_template("signup.html", form=form)
+    else:
+        print("Adding User to Database")
+        #password should be hashed
+        new_user = User(email=email, username=username, password=password,grade=grade,school=school)
+        db.session.add(new_user)
+        db.session.commit()
+        return index()
+    """
--- flash_learning/models/forms.py
+++ flash_learning/models/forms.py
@@ -1,43 +1,35 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField,IntegerField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Email, ValidationError
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+from flash_learning.models.user import User
+
 
 class LoginForm(FlaskForm):
-    username = StringField('username')
-    password = PasswordField('password')
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    submit = SubmitField("Sign In")
+
 
 class SignupForm(FlaskForm):
-    username = StringField('username',validators=[DataRequired()])
-    password = PasswordField('password',validators=[DataRequired()])
-    email = StringField('email',validators=[DataRequired()])
-    school =  StringField(label='school',validators=[DataRequired()])
-    grade = IntegerField('grade',validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    school = StringField(label='school', validators=[DataRequired()])
+    grade = IntegerField('grade', validators=[DataRequired()])
 
-class User(UserMixin,db.Model):
-    __tablename__ = 'usertable'
-    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    username = db.Column(db.String(1000))
-    grade = db.Column(db.Integer)
-    school = db.Column(db.String(100), unique=True)
+    def validate_username(self, username):
+        """Check if the username is already taken."""
 
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Please use a different username.")
 
-    def get_id(self):
-        return self.alternative_id
+    def validate_email(self, email):
+        """Check if the email is already tied to an account."""
 
-    @property
-    def is_authenticated(self):
-        """Return True if the user is authenticated."""
-        self.authenticated=False
-        return self.authenticated
-
-    def get(self):
-        return self
-
-
-
+        email = User.query.filter_by(email=email.data).first()
+        if email is not None:
+            raise ValidationError("Please use a different email address.")
