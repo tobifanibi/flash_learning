64e66a26acdbe69fc54b12a917d500d3195058a0
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2020-12-19 19:58:47 -0500

    Update forms with new fields.

--- flash_learning/main/forms.py
+++ flash_learning/main/forms.py
@@ -1,10 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Email, ValidationError
-from flask_login import UserMixin
-from flask_sqlalchemy import SQLAlchemy
 
-from flash_learning.models.user import User
+from flash_learning.models.student import Student
 
 
 class LoginForm(FlaskForm):
@@ -14,22 +12,24 @@ class LoginForm(FlaskForm):
 
 
 class SignupForm(FlaskForm):
+    first_name = StringField('first_name', validators=[DataRequired()])
+    last_name = StringField('last_name', validators=[DataRequired()])
     username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(), Email()])
-    school = StringField(label='school', validators=[DataRequired()])
     grade = IntegerField('grade', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    submit = SubmitField("Sign Up")
 
     def validate_username(self, username):
         """Check if the username is already taken."""
 
-        user = User.query.filter_by(username=username.data).first()
+        user = Student.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError("Please use a different username.")
 
     def validate_email(self, email):
         """Check if the email is already tied to an account."""
 
-        email = User.query.filter_by(email=email.data).first()
+        email = Student.query.filter_by(email=email.data).first()
         if email is not None:
             raise ValidationError("Please use a different email address.")
