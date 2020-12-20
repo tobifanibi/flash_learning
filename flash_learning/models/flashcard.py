28f63e40a6a20c49765b70c1a00ff800114113e4
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2020-12-19 19:59:21 -0500

    Add additional ORMs for flashcards.

--- flash_learning/models/flashcard.py
+++ flash_learning/models/flashcard.py
@@ -1,23 +1,57 @@
-from datetime import datetime
-
 from flash_learning import db
 
 
-# Create a model for a Flashcard
-class Flashcard(db.Model):
+class Grade(db.Model):
+    """Database model for a grade from K-8 with multiple subjects."""
 
-    # Add a primary key with a id field
     id = db.Column(db.Integer, primary_key=True)
+    grade = db.Column(db.String, index=True)
+    subjects = db.relationship("Subject", backref="grade", lazy=True)
 
-    # Add a column to the database with the string of the front side of our card
-    content_front_side = db.Column(db.String(200), nullable=False)
+    def __init__(self, grade):
+        self.grade = grade
 
-    # Add a column to the database with the string of the back side of our card
-    content_back_side = db.Column(db.String(200), nullable=False)
 
-    # Add date of creation for bookeeping, with a default creation time of whenever it was created
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+class Subject(db.Model):
+    """Database model for a single subject with multiple decks."""
 
-    # Add a repprint function just to make it easier to reference this flashcard
+    id = db.Column(db.Integer, primary_key=True)
+    grade_id = db.Column(db.Integer, db.ForeignKey("grade.id"), nullable=False)
+    name = db.Column(db.String, index=True)
+    decks = db.relationship("Deck", backref="subject", lazy=True)
+
+    def __init__(self, name, grade_id):
+        self.name = name
+        self.grade_id = grade_id
+
+
+class Deck(db.Model):
+    """Database model for a single deck with multiple flashcards."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    subject_id = db.Column(db.Integer, db.ForeignKey("subject.id"), nullable=False)
+    name = db.Column(db.String, index=True)
+    cards = db.relationship("Flashcard", backref="deck", lazy=True)
+
+    def __init__(self, name, subject_id):
+        self.name = name
+        self.subject_id = subject_id
+
+
+class Flashcard(db.Model):
+    """Database model for a single flashcard with a question and answer."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    deck_id = db.Column(db.Integer, db.ForeignKey("deck.id"), nullable=False)
+    number = db.Column(db.Integer, index=True)
+    question = db.Column(db.String)
+    answer = db.Column(db.String)
+
+    def __init__(self, number, question, answer, deck_id):
+        self.number = number
+        self.question = question
+        self.answer = answer
+        self.deck_id = deck_id
+
     def __repr__(self):
-        return '<Flashcard %r>' % self.id
+        return f"{self.number}) {self.question} : {self.answer}"
