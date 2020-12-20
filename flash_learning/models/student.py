f991006c586af3ac27680cad04814618f2a2cb93
Mohamed Al-Hussien <sn95racing.v6@gmail.com>
2020-12-19 19:59:40 -0500

    Add init methods to student model.

--- flash_learning/models/student.py
+++ flash_learning/models/student.py
@@ -7,26 +7,31 @@ from flask_login import UserMixin
 @login_manager.user_loader
 def load_user(id):
     """Load a user from the database."""
-    # print(User.query.get(1))
-    return User.query.filter_by(alternative_id=id).first()
+
+    return Student.query.filter_by(id=id).first()
 
 
-
-class User(UserMixin, db.Model):
+class Student(UserMixin, db.Model):
     """Student data model."""
 
-    __tablename__ = 'usertable'
     #static key for user
     # primary keys are required by SQLAlchemy
     id = db.Column(db.Integer, primary_key=True)
-    #login key for user, changed when changing password
-    alternative_id = db.Column(db.Integer,index=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(128))
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
     username = db.Column(db.String(64), index=True, unique=True)
     grade = db.Column(db.Integer, index=True)
-    school = db.Column(db.String(100), index=True)
     activated = db.Column(db.Boolean, default=False)
+
+    def __init__(self, first_name, last_name, username, grade, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.email = email
+        self.grade = grade
+        self.set_password(password)
 
     def __repr__(self):
         return f"<User {self.username}>"
@@ -40,7 +45,8 @@ class User(UserMixin, db.Model):
         """Check the password hash from the database against the given password."""
 
         return check_password_hash(self.password, password)
-    def get_id(self):
-        return self.alternative_id
 
 
+class StudentFlashcard(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    last_reviewed = db.Column(db.DATE, index=True)
