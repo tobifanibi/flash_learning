49814a13d7de09245aba9849c53357fde0f173c0
Asa Holland <hollandasa@gmail.com>
2020-12-26 17:52:38 -0800

    added score line graph to stats page
    Added javascript stats graph (score per month) to html stats page (will need to be linked to database)

--- flash_learning/static/js/stats_graph.js
+++ flash_learning/static/js/stats_graph.js
@@ -1,39 +1,30 @@
+
+// Create new line chart using chart.js
 new Chart(document.getElementById("line-chart"), {
   type: 'line',
   data: {
-    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-    datasets: [{ 
-        data: [86,114,106,106,107,111,133,221,783,2478],
-        label: "Africa",
+
+  	// Separate vertical lines by months
+    labels: ['January','February','March','April','May','June','July','August','September','October', 'November', 'December'],
+    
+    // Graph these datasets (could add more, possibly the average user?)
+    datasets: [
+
+    // Data for the current user
+    { 
+        data: [0,0,22,24,56,24,26,96,87,81, 56, 34],
+        label: "Your Score",
         borderColor: "#3e95cd",
-        fill: false
-      }, { 
-        data: [282,350,411,502,635,809,947,1402,3700,5267],
-        label: "Asia",
-        borderColor: "#8e5ea2",
-        fill: false
-      }, { 
-        data: [168,170,178,190,203,276,408,547,675,734],
-        label: "Europe",
-        borderColor: "#3cba9f",
-        fill: false
-      }, { 
-        data: [40,20,10,16,24,38,74,167,508,784],
-        label: "Latin America",
-        borderColor: "#e8c3b9",
-        fill: false
-      }, { 
-        data: [6,3,2,2,7,26,82,172,312,433],
-        label: "North America",
-        borderColor: "#c45850",
         fill: false
       }
     ]
   },
+
+  // Add an overall title to the line graph
   options: {
     title: {
       display: true,
-      text: 'World population per region (in millions)'
+      text: 'Number of Successful Flaschcards Learned Per Month'
     }
   }
 });
